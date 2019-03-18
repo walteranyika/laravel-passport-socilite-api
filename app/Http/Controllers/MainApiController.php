bad8fe87d6ba085edf069925f69a80cbd2432ddd
Walter Sanchez <walteranyika@gmail.com>
2019-03-18 17:17:37 +0300

    Restaurant App V1

---
+++ app/Http/Controllers/MainApiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainApiController extends Controller
+{
+    public function  getRestaurants(){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->group(function (){
+    Route::get('/restaurants', 'MainApiController@getRestaurants');
+
+
+});
